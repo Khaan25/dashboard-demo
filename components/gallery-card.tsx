3ed9791fdf6af44c0b9dec2c69d7f4edbb9b3915
Khaan25 <ziarzai@hotmail.com>
2025-01-31 20:13:20 +0500

    feat: design adjustments as per Vandana

--- components/gallery-card.tsx
+++ components/gallery-card.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export function GalleryCard({ item }: { item: GalleryItem }) {
   return (
     <div className="group bg-card overflow-hidden">
-      <div className="p-5 bg-gray-100 rounded-md border border-border">
+      <div className="px-5 py-16 bg-[#f3f4f6] rounded-md">
         <div className="relative aspect-square min-h-96 w-full rounded-md overflow-hidden">
           <Image src={item.imageUrl || '/placeholder.svg'} width={400} height={500} alt={item.title} className="object-contain h-full transition-transform duration-300 ease-in-out group-hover:scale-110" />
           {/* <div className="absolute inset-0 bg-black/30 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
